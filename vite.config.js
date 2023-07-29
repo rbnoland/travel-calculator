23cc8b361be78b2b02d63d3dc1d7e88c520a96a7
Benjamin Noland <benjaminnoland93@gmail.com>
2023-07-28 22:49:46 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { resolve } from "path";
 
+const root = resolve(__dirname, "src");
+const publicDir = resolve(__dirname, "public");
+const outDir = resolve(__dirname, "dist");
+
 export default {
-  root: resolve(__dirname, "src"),
-  publicDir: resolve(__dirname, "public"),
+  root,
+  publicDir,
   build: {
-    outDir: "../dist"
+    outDir,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        nested: resolve(root, 'nested/index.html'),
+      },
+    },
   },
   server: {
     port: 3000
