13b9a682997f773f5d0431479d864e349b7d2cc3
Benjamin Noland <benjaminnoland93@gmail.com>
2023-07-28 20:06:21 -0400

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { resolve } from "path";
+
+export default {
+  root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
+  build: {
+    outDir: "../dist"
+  },
+  server: {
+    port: 3000
+  }
+};
